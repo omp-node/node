87b71852963a3ee18b301c1596874c2d617f11db
iAmir <hhm6@yahoo.com>
2024-06-26 19:00:34 +0330

    proper initializers

--- initialize.js
+++ initialize.js
@@ -4,6 +4,7 @@ import {
   GangZonePool,
   MenuPool,
   ObjectPool,
+  PerPlayerEntityPool,
   PickupPool,
   PlayerObjectPool,
   PlayerPool,
@@ -13,72 +14,69 @@ import {
 } from "./pools.js";
 import * as constants from "./constants.js";
 
-omp.log = __internal_omp.log;
-omp.resource = __internal_resource;
-
-function onResourceStart(error) {
+async function onResourceStart(badRet, error) {
   if (!error) {
     omp.players = new PlayerPool();
     for (let i = 0; i < constants.PLAYER_POOL_SIZE; i++) {
       try {
         omp.players.add_INTERNAL_UNSAFE(i, true);
-      } catch (e) {}
+      } catch (e) { }
     }
 
     omp.vehicles = new VehiclePool();
     for (let i = 0; i < constants.VEHICLE_POOL_SIZE; i++) {
       try {
         omp.vehicles.add_INTERNAL_UNSAFE(i, true);
-      } catch (e) {}
+      } catch (e) { }
     }
 
     omp.objects = new ObjectPool();
     for (let i = 0; i < constants.OBJECT_POOL_SIZE; i++) {
       try {
         omp.objects.add_INTERNAL_UNSAFE(i, true);
-      } catch (e) {}
+      } catch (e) { }
     }
 
     omp.textdraws = new TextDrawPool();
     for (let i = 0; i < constants.GLOBAL_TEXTDRAW_POOL_SIZE; i++) {
       try {
         omp.textdraws.add_INTERNAL_UNSAFE(i, true);
-      } catch (e) {}
+      } catch (e) { }
     }
 
     omp.pickups = new PickupPool();
     for (let i = 0; i < constants.PICKUP_POOL_SIZE; i++) {
       try {
         omp.pickups.add_INTERNAL_UNSAFE(i, true);
-      } catch (e) {}
+      } catch (e) { }
     }
 
     omp.gangzones = new GangZonePool();
     for (let i = 0; i < constants.GANG_ZONE_POOL_SIZE; i++) {
       try {
         omp.gangzones.add_INTERNAL_UNSAFE(i, true);
-      } catch (e) {}
+      } catch (e) { }
     }
 
     omp.textlabels = new TextLabelPool();
     for (let i = 0; i < constants.TEXT_LABEL_POOL_SIZE; i++) {
       try {
         omp.textlabels.add_INTERNAL_UNSAFE(i, true);
-      } catch (e) {}
+      } catch (e) { }
     }
 
     omp.actors = new ActorPool();
     for (let i = 0; i < constants.ACTOR_POOL_SIZE; i++) {
       try {
         omp.actors.add_INTERNAL_UNSAFE(i, true);
-      } catch (e) {}
+      } catch (e) { }
     }
 
     omp.menus = new MenuPool();
     for (let i = 0; i < constants.MENU_POOL_SIZE; i++) {
       try {
         omp.menus.add_INTERNAL_UNSAFE(i, true);
-      } catch (e) {}
+      } catch (e) { }
     }
 
     omp.playerObjects = new PerPlayerEntityPool();
@@ -89,7 +87,7 @@ function onResourceStart(error) {
       for (let i = 0; i < constants.OBJECT_POOL_SIZE; i++) {
         try {
           pool.add_INTERNAL_UNSAFE(i, true);
-        } catch (e) {}
+        } catch (e) { }
       }
 
       entries.set(player.getID(), pool);
@@ -103,7 +101,7 @@ function onResourceStart(error) {
       for (let i = 0; i < constants.TEXT_LABEL_POOL_SIZE; i++) {
         try {
           pool.add_INTERNAL_UNSAFE(i, true);
-        } catch (e) {}
+        } catch (e) { }
       }
 
       entries.set(player.getID(), pool);
@@ -117,13 +115,20 @@ function onResourceStart(error) {
       for (let i = 0; i < constants.TEXT_LABEL_POOL_SIZE; i++) {
         try {
           pool.add_INTERNAL_UNSAFE(i, true);
-        } catch (e) {}
+        } catch (e) { }
       }
 
       entries.set(player.getID(), pool);
     });
   }
+
+  const eventEmitter = __internal_omp.eventEmitter;
+  const listeners = eventEmitter.listeners("resourceStart");
+  for await (const listener of listeners) {
+    await listener(error);
+  }
 }
 
-omp.removeListener("resourceStart", onResourceStart);
-omp.on("resourceStart", onResourceStart);
+const eventEmitter_raw = __internal_omp.eventEmitter_raw;
+eventEmitter_raw.removeListener("resourceStart", onResourceStart);
+eventEmitter_raw.on("resourceStart", onResourceStart);
